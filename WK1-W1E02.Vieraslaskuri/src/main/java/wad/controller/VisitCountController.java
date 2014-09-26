bd680890f862b5b7c034eeeeda0b5c314372fc04
Jaakko Komulainen <jaakko@ruoste.tontut.fi>
2014-09-27 02:03:52 +0300

    wk2

--- WK1-W1E02.Vieraslaskuri/src/main/java/wad/controller/VisitCountController.java
+++ WK1-W1E02.Vieraslaskuri/src/main/java/wad/controller/VisitCountController.java
@@ -2,14 +2,15 @@ package wad.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @ResponseBody
 public class VisitCountController {
-    @RequestMapping("/tervehdi")
-    public String hello(@RequestParam String nimi) {
-        return "Hei " + nimi;
+    int visitCount = 0;
+    @RequestMapping("/visits")
+    public int Visits(){
+        this.visitCount++;
+        return this.visitCount;
     }
 }
