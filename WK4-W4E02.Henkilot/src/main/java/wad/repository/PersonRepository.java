9ec01cd5acfd5f88aaf05fd9b6a9fa4d3fac9e33
Jaakko Komulainen <komulainenjaakko@gmail.com>
2014-10-18 19:26:58 +0300

    added new repository PersonRepository and controller PersonController

---
+++ WK4-W4E02.Henkilot/src/main/java/wad/repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package wad.repository;
+
+import wad.domain.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    @Override
+    public void delete(Long id);
+
+}
