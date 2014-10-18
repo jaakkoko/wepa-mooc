be834341c21841118c3842b0e7af3228bfc9c86d
Jaakko Komulainen <komulainenjaakko@gmail.com>
2014-10-18 20:15:53 +0300

    adding persons

--- WK4-W4E02.Henkilot/src/main/java/wad/controller/PersonController.java
+++ WK4-W4E02.Henkilot/src/main/java/wad/controller/PersonController.java
@@ -1,11 +1,24 @@
 package wad.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import wad.domain.Person;
+import wad.repository.PersonRepository;
 
 @RestController
 @RequestMapping("/persons")
 public class PersonController{
 
+    @Autowired
+    PersonRepository personRepository;
+    
+    @RequestMapping(method=RequestMethod.POST)
+    public void addPerson(@RequestBody Person person){
+        personRepository.save(person);
+    }
+    
     
 }
