052d9ad224d3111431265ac22b11c954b3d710c0
Jaakko Komulainen <komulainenjaakko@gmail.com>
2014-10-18 19:29:16 +0300

    added attributes and get/set methods to Person.java

--- WK4-W4E02.Henkilot/src/main/java/wad/domain/Person.java
+++ WK4-W4E02.Henkilot/src/main/java/wad/domain/Person.java
@@ -1,5 +1,6 @@
 package wad.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import org.springframework.data.jpa.domain.AbstractPersistable;
@@ -8,5 +9,55 @@ import org.springframework.data.jpa.domain.AbstractPersistable;
 @Table(name="Person")
 public class Person extends AbstractPersistable<Long>{
 
+    @Column(unique=true)
+    private String name;
+    
+    @Column(unique=true)
+    private String username;
+    
+    @Column(unique=true)
+    private String password;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
