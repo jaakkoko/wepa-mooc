75eb04c358ddd0c05d615f33b055d04612fda4e4
Jaakko Komulainen <komulainenjaakko@gmail.com>
2014-10-20 22:56:39 +0300

    test adding and deleting actor

--- WK4-W4E05.ElokuvatietokantaRedux/src/test/java/wad/selenium/ActorTest.java
+++ WK4-W4E05.ElokuvatietokantaRedux/src/test/java/wad/selenium/ActorTest.java
@@ -1,4 +1,5 @@
 package wad.selenium;
+import java.util.List;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
@@ -7,15 +8,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.springframework.boot.SpringApplication;
-import wad.Application;
-
 
 public class ActorTest{
     
     private WebDriver driver;
 
     private final String ACTOR_URL = "http://localhost:8080/actors";
+
     
     @Before
     public void setUp() {
@@ -31,14 +30,20 @@ public class ActorTest{
     public void addingAndDeletingActor() {
         driver.get(ACTOR_URL);
         assertFalse(driver.getPageSource().contains("Van Damme"));
-        
       
         WebElement element = driver.findElement(By.id("name"));
         element.sendKeys("Van Damme");
         element.submit();
         assertTrue(driver.getPageSource().contains("Van Damme"));
         
-       
+        List<WebElement>els = driver.findElements(By.id("command"));
+        for(WebElement e : els)e.submit();
         
+        assertFalse(driver.getPageSource().contains("Van Damme"));
+        
+    }
+    
+    public void addingAndDeletingTwoActors(){
+    
     }
 }
