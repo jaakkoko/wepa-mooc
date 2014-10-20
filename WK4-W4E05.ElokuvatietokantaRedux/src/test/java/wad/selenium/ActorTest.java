7751d604707618273a451f2d1963dc40bc934195
Jaakko Komulainen <komulainenjaakko@gmail.com>
2014-10-20 21:42:54 +0300

    initilized selenium tests for actors

---
+++ WK4-W4E05.ElokuvatietokantaRedux/src/test/java/wad/selenium/ActorTest.java
@@ -0,0 +1,48 @@
+package wad.selenium;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.boot.SpringApplication;
+import wad.Application;
+
+
+public class ActorTest{
+    
+    private WebDriver driver;
+
+    private final String ACTOR_URL = "http://localhost:8080/actors";
+    
+    @Before
+    public void setUp() {
+        // käynnistää Spring-sovelluksen -- JSP-kirjastojen lataaminen
+        // tällä hetkellä kuitenkin buginen, joten ajetaan testit 
+        // manuaalisesti käynnistettyyn sovellukseen
+
+        SpringApplication.run(Application.class);
+        this.driver = new HtmlUnitDriver();
+    }
+
+    @Test
+    public void onceBobSubmittedSignupSuccessful() {
+        // haetaan haluttu osoite (aiemmin määritelty muuttuja, esim.
+        // http://localhost:8080/signup )
+        driver.get(ACTOR_URL);
+        assertFalse(driver.getPageSource().contains("Van Damme"));
+        
+        
+        // haetaan kenttä nimeltä tunnus
+        WebElement element = driver.findElement(By.name("name"));
+        element.sendKeys("Van Damme");
+        element.submit();
+        
+        assertTrue(driver.getPageSource().contains("Van Damme"));
+        
+       
+        
+    }
+}
