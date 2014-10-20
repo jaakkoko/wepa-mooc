84a31a11ba0b05514ac28fdc763987961745f230
Jaakko Komulainen <komulainenjaakko@gmail.com>
2014-10-20 20:46:35 +0300

    get test for aircraft controller

--- WK4-W4E04.LentokentatRedux/src/test/java/wad/controller/AircraftControllerTest.java
+++ WK4-W4E04.LentokentatRedux/src/test/java/wad/controller/AircraftControllerTest.java
@@ -4,12 +4,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -34,8 +34,14 @@ public class AircraftControllerTest{
     }
 
     @Test
-    public void statusOk() throws Exception {
+    public void getAircrafts() throws Exception {
         mockMvc.perform(get(API_URI))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("aircrafts"))
+                .andExpect(model().attributeExists("airports"));
     }
+   
+    
+    
+    
 }
