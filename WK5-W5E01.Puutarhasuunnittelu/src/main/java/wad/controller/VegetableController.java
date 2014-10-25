c498dd764e225ec7d564c4292f12d426dad56f22
Jaakko Komulainen <komulainenjaakko@gmail.com>
2014-10-25 23:11:50 +0300

    Get method for vegetable controller

--- WK5-W5E01.Puutarhasuunnittelu/src/main/java/wad/controller/VegetableController.java
+++ WK5-W5E01.Puutarhasuunnittelu/src/main/java/wad/controller/VegetableController.java
@@ -1,18 +1,32 @@
 package wad.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import wad.domain.Vegetable;
+import wad.repository.VegetableRepository;
 
 @Controller
 @RequestMapping("/vegetables")
 public class VegetableController {
 
+    @Autowired
+    private VegetableRepository vgRepo;
+    
+    
+    @ModelAttribute("vegetable")
+    public Vegetable vegetable(){
+        return new Vegetable();
+    }
+    
     @RequestMapping(method = RequestMethod.GET)
     public String getVegetables(Model model) {
-        
-        
+        model.addAttribute("message", "Vegetables!");
+        model.addAttribute("vegetables",vgRepo.findAll());
+        model.addAttribute("vegetable");
         return "vegetables";
     }
 }
