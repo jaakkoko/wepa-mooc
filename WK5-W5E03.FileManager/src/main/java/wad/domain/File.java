875e823694a1f75ae09c4dba0246661eed2d51b7
Jaakko Komulainen <komulainenjaakko@gmail.com>
2014-10-28 00:19:48 +0200

    attributes for file

--- WK5-W5E03.FileManager/src/main/java/wad/domain/File.java
+++ WK5-W5E03.FileManager/src/main/java/wad/domain/File.java
@@ -1,10 +1,19 @@
 package wad.domain;
 
+import javax.persistence.Basic;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Lob;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
 public class File extends AbstractPersistable<Long> {
     
-    
+    private String name;
+    private String mediaType;
+    private Long size;
+
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    private byte[] content;
 }
