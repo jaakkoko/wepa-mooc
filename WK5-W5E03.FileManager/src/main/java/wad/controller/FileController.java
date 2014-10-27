f4ec32684426d64f0a814ce18f806c4ba01bb7d1
Jaakko Komulainen <komulainenjaakko@gmail.com>
2014-10-28 00:07:00 +0200

    added get method for file controller

--- WK5-W5E03.FileManager/src/main/java/wad/controller/FileController.java
+++ WK5-W5E03.FileManager/src/main/java/wad/controller/FileController.java
@@ -1,8 +1,16 @@
 package wad.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 @Controller
 @RequestMapping("/files")
 public class FileController{
 
-
+    @RequestMapping(method=RequestMethod.GET)
+    public String getFiles(Model model){
+        return "redirect:/files";
+    }
 }
