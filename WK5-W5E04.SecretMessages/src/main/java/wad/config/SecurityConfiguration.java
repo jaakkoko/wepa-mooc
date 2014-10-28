082e67de898ae686d6358fccef4f56b7ac1f2c61
Jaakko Komulainen <komulainenjaakko@gmail.com>
2014-10-28 22:38:44 +0200

    security configuration class

---
+++ WK5-W5E04.SecretMessages/src/main/java/wad/config/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package wad.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebMvcSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+}
